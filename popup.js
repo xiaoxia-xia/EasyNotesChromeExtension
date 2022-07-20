0d5794f1afd2cc2dad3670680173268b8b65d3cb
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-07-19 21:18:26 -0700

    Update popup.js
    Fixed undefined bug
    
    Co-Authored-By: Guan Li <46405838+splotchysnow@users.noreply.github.com>

--- popup.js
+++ popup.js
@@ -2,30 +2,45 @@
 let changeColor = document.getElementById("changeColor");
 
 let value_ = "";
+chrome.storage.sync.set({value_});
 
+// Represents the textBox.
 let input_ = document.getElementById("input");
 // input_.value += "FWIOEJFIOWEJFOIJWEIOFJWEOIJFOWIEJFOIWEIJO";
-console.log(input_.value);
+// console.log(input_.value);
+
+/**
+ * Stores the value whenever the textbox is clicked.
+ */
+input_.addEventListener("click", async() =>
+{ 
+  value_ = input_.value;
+  chrome.storage.sync.set({value_});
+}
+);
+
+/**
+ * retreives the value from the chrome storage.
+ */
+chrome.storage.sync.get("value_", ({ value_ }) => {
+  //To avoid bug for undefined?
+  // if (value_ == undefined){
+  //   value_ = '';
+  // }
+  // else if (value_ == null){
+  //   value_ = '';
+  // }
+  input_.value = value_;
+});
 
 
+
+// The color stuff..
 chrome.storage.sync.get("color", ({ color }) => {
   //Set the color of the button, ( don't effect the function tho. )
   // console.log("Hello World");
   changeColor.style.backgroundColor = color;
 });
-
-
-input_.addEventListener("click", async() =>{
-  // console.log(input_.value);
-  value_ = input_.value;
-  chrome.storage.sync.set({value_});
-});
-
-
-chrome.storage.sync.get("value_", ({ value_ }) => {
-  input_.value = value_;
-});
-
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
