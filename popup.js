47a3ecf7b4636a06322a4aa463164bf2db6bc897
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-07-20 21:06:11 -0700

    bottom update
    Co-Authored-By: Guan Li <46405838+splotchysnow@users.noreply.github.com>

--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
 // Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+// let changeColor = document.getElementById("changeColor");
 
 let value_ = "";
 chrome.storage.sync.set({value_});
 
 // Represents the textBox.
-let input_ = document.getElementById("input");
+let input_ = document.getElementById("notes_input");
 // input_.value += "FWIOEJFIOWEJFOIJWEIOFJWEOIJFOWIEJFOIWEIJO";
 // console.log(input_.value);
 
@@ -35,33 +35,33 @@ chrome.storage.sync.get("value_", ({ value_ }) => {
 
 
 
-// The color stuff..
-chrome.storage.sync.get("color", ({ color }) => {
-  //Set the color of the button, ( don't effect the function tho. )
-  // console.log("Hello World");
-  changeColor.style.backgroundColor = color;
-});
+// // The color stuff..
+// chrome.storage.sync.get("color", ({ color }) => {
+//   //Set the color of the button, ( don't effect the function tho. )
+//   // console.log("Hello World");
+//   changeColor.style.backgroundColor = color;
+// });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  // console.log("Hello World");
+// // When the button is clicked, inject setPageBackgroundColor into current page
+// changeColor.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//   // console.log("Hello World");
 
-    //Function calls.
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
+//     //Function calls.
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: setPageBackgroundColor,
+//   });
+// });
 
 
 
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    //Incharge of the actual thing that controls the background of the chrome page.
-    document.body.style.backgroundColor = color;
-    // console.log("Hello World");
-  });
-}
+// // The body of this function will be execuetd as a content script inside the
+// // current page
+// function setPageBackgroundColor() {
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     //Incharge of the actual thing that controls the background of the chrome page.
+//     document.body.style.backgroundColor = color;
+//     // console.log("Hello World");
+//   });
+// }
