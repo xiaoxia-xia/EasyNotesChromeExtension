d8238a21551d6562779b105a07f64c0309a7612e
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-07-20 23:10:50 -0700

    updated
    Co-Authored-By: Guan Li <46405838+splotchysnow@users.noreply.github.com>

---
+++ background.js
@@ -0,0 +1,6 @@
+let color = '#a73a53';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
